590f28439e18c58e961aaa96dae1b1261e778bfa
Timothy Mao <tmao042@uottawa.ca>
2025-12-08 21:05:47 -0500

    untested but i added BLIP to generate text descriptions of all the images and then save it to big_data.csv,,, also i added .convert('RGB') to the pipeline of image shits, and added an img_only parameter to generate embeddings func

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-from transformers import BertTokenizer, BertModel, CLIPModel, CLIPImageProcessor, VisualBertModel
+from transformers import BertTokenizer, BertModel, CLIPModel, CLIPImageProcessor, VisualBertModel, BlipProcessor, BlipForConditionalGeneration
 from PIL import Image, ImageFile
 import torch
 from torch.utils.data import DataLoader
@@ -20,8 +20,13 @@ clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(DEVICE
 
 vb_model = VisualBertModel.from_pretrained("uclanlp/visualbert-vqa-coco-pre")
 
+# for img2text
+blip_processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+blip_model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
 NUM_PATCHES = None
 TEXT_SEQUENCE_LENGTH = None
+IMAGES_LIST = None
 # these "constants" ^ are set on a run of get_embeddings
 
 class VBDataset(torch.utils.data.Dataset):
@@ -39,12 +44,12 @@ class VBDataset(torch.utils.data.Dataset):
             "attention_mask": self.text_inputs["attention_mask"][idx],
         }
 
-
-def get_embeddings(img_path:Path, labels_path:Path, overwrite=False, test_cap=0):
-    global NUM_PATCHES, TEXT_SEQUENCE_LENGTH
+def get_embeddings(img_path:Path, labels_path:Path, img_only=False, overwrite=False, test_cap=0):
+    global NUM_PATCHES, TEXT_SEQUENCE_LENGTH, IMAGES_LIST
     ''' Saves embeddings as image_embeddings.pt and text input as text_inputs.pt
     will retrieve an already generated + saved .pt (if it exists) if overwrite = False, otherwise will generate a new .pt
-    if test_cap > 0 , will only iterate thru that many files maximum (for testing purposes)
+    if test_cap > 0 , will only iterate thru that many files maximum (for testing purposes).
+    img_only is for uhhhhh just generating the IMAGES_LIST constant mainly.
     Returns the image embeds and the text inputs
     '''
     data_path = get_root_dir() / 'data'
@@ -59,7 +64,7 @@ def get_embeddings(img_path:Path, labels_path:Path, overwrite=False, test_cap=0)
     big_data = {"img_name":[]} # we need the image names in order so we know which embeds are for which image
     imgs = []
     texts = []
-    labels = pd.read_csv(labels_path)
+    if not img_only: labels = pd.read_csv(labels_path)
     i = 0 # for test_cap, for testing purposes
     total_images = len([file for file in os.listdir(img_path)]) if test_cap <= 0 else test_cap
     progress_check = 0.05*total_images # i want a progress check every 5% so i know it's not frozen
@@ -70,18 +75,19 @@ def get_embeddings(img_path:Path, labels_path:Path, overwrite=False, test_cap=0)
         i += 1
         # get a file, add it to imgs, get its text and add it to text
         try:
-            img = Image.open(file)
+            img = Image.open(file).convert('RGB')
             img.load()
             imgs.append(img)
             #print("image appended")
-            txt = labels.loc[labels['image_name'] == file.name]['text_corrected'].iloc[0]
-            if type(txt) != str:
-                print(f"Found non-string \"{str(txt)}\" when searching for {file.name}, converting to string")
-                if np.isnan(txt):
-                    txt = "" 
-                else:
-                    txt = str(txt)
-            texts.append(txt)
+            if not img_only:
+                txt = labels.loc[labels['image_name'] == file.name]['text_corrected'].iloc[0]
+                if type(txt) != str:
+                    print(f"Found non-string \"{str(txt)}\" when searching for {file.name}, converting to string")
+                    if np.isnan(txt):
+                        txt = "" 
+                    else:
+                        txt = str(txt)
+                texts.append(txt)
             big_data["img_name"].append(file.name)
         except Exception as e:
             print(f"had an error boyo, with {file}, {e}")
@@ -91,27 +97,31 @@ def get_embeddings(img_path:Path, labels_path:Path, overwrite=False, test_cap=0)
             print(f'{i*100.0/total_images:.2f}% ({i}/{total_images})')
             last_prog = i//progress_check
         
+    pd.DataFrame(big_data).to_csv(data_path/"big_data.csv", index=False)
+    IMAGES_LIST = imgs
     print("DONE")
     print(len(imgs))
     print(len(texts))
 
-    img_in = processor(images=imgs, return_tensors='pt').to(DEVICE)
-    text_in = tokenizer(text=texts, padding="max_length", max_length=512, truncation=True, return_tensors='pt').to(DEVICE)
-    text_in = {k: v for k, v in text_in.items()}
-    img_embeds = None
-    with torch.no_grad(): # save computation power and memory
-        # get em
-        img_embeds = clip_model.vision_model(**img_in).last_hidden_state
-    if img_embeds == None: raise Exception('embeds not correctly generated')
-    # set constants to use for visualbert
-    _, NUM_PATCHES, _ = img_embeds.shape
-    _, TEXT_SEQUENCE_LENGTH = text_in['input_ids'].shape
-    # save em
-    torch.save(img_embeds, data_path/"image_embeddings.pt")
-    torch.save(text_in, data_path/"text_inputs.pt")
-    pd.DataFrame(big_data).to_csv(data_path/"big_data.csv", index=False)
-    # return em
-    return img_embeds, text_in
+    if not img_only:
+        img_in = processor(images=imgs, return_tensors='pt').to(DEVICE)
+        text_in = tokenizer(text=texts, padding="max_length", max_length=512, truncation=True, return_tensors='pt').to(DEVICE)
+        text_in = {k: v for k, v in text_in.items()}
+        img_embeds = None
+        with torch.no_grad(): # save computation power and memory
+            # get em
+            img_embeds = clip_model.vision_model(**img_in).last_hidden_state
+        if img_embeds == None: raise Exception('embeds not correctly generated')
+        # set constants to use for visualbert
+        _, NUM_PATCHES, _ = img_embeds.shape
+        _, TEXT_SEQUENCE_LENGTH = text_in['input_ids'].shape
+        # save em
+        torch.save(img_embeds, data_path/"image_embeddings.pt")
+        torch.save(text_in, data_path/"text_inputs.pt")
+        # return em
+        return img_embeds, text_in
+    else:
+        return
 
 def feed_VisualBERT(img_embeds, text_inputs, overwrite = False):
     proj = torch.nn.Linear(768, 2048).to(DEVICE)
@@ -159,3 +169,36 @@ def feed_VisualBERT(img_embeds, text_inputs, overwrite = False):
     torch.save(final_output, data_path / 'visualbert_output.pt')
 
     return final_output
+
+def feed_BLIP(img_path, overwrite=False, test_cap=0):
+    global IMAGES_LIST
+    '''
+    Docstring for feed_BLIP
+    
+    :param img_path: the path with all the images
+    :type img_path: Path
+    :param overwrite: forces new files to be made even. if false, will retrieve already existing files (if they exist)
+    :param test_cap: will process all images in path unless specified to end early (mainly for testing purposes)
+    '''
+    data_path = get_root_dir() / 'data'
+    if overwrite == False: # if not forcing an overwrite, grab existing files
+        if (data_path / 'image_embeddings.pt').exists() and (data_path / 'text_inputs.pt').exists(): # ...if they exist
+            text_descriptions = pd.read_csv(data_path / 'text_descriptions.csv')
+            return text_descriptions
+        
+    if IMAGES_LIST == None:
+        get_embeddings(img_path, Path(None), img_only=True, overwrite=True)
+    
+    big_data = pd.read_csv(data_path/'big_data.csv')
+    text_descriptions = []
+    batch_size = 16
+    for i in range(0, len(IMAGES_LIST), batch_size):
+        batch_images = IMAGES_LIST[i : i + batch_size]
+        blip_in = blip_processor(images=batch_images, return_tensors="pt", padding=True).to(DEVICE)
+        blip_out = blip_model.generate(**blip_in, max_new_tokens=50)
+        captions = [blip_processor.decode(out, skip_special_tokens=True) for out in blip_out]
+        text_descriptions += captions
+    big_data['text_descriptions'] = text_descriptions
+    pd.DataFrame(big_data).to_csv(data_path/"big_data.csv", index=False)
+    
+    return text_descriptions
